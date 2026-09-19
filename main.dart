6e1d8d0a499fd079a3ac6c9f30d380e2475f933c
imranprince1358-rgb <imranprince1358@gmail.com>
2026-09-19 15:15:52 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:firebase_core/firebase_core.dart';
- => runApp(const ApnaSheharApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ApnaSheharApp());
+}
 
 class ApnaSheharApp extends StatelessWidget {
   const ApnaSheharApp({super.key});
